c2c5feba25ae34c60e6c16d41e539cede2a05e0b
sinfounad <ivanpin971@hotmail.com>
2020-06-19 15:01:09 -0500

    Updates

---
+++ precache-manifest.2007825c959bf7200840f3b826d39adc.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665faf58da21c8b215db0d0012613bb8",
+    "url": "/Quiroga-sector1/index.html"
+  },
+  {
+    "revision": "c84cff7ce9a7105f9cfb",
+    "url": "/Quiroga-sector1/static/css/main.37f45666.chunk.css"
+  },
+  {
+    "revision": "4b65a20e2f770e1a956e",
+    "url": "/Quiroga-sector1/static/js/2.760ae38c.chunk.js"
+  },
+  {
+    "revision": "748d5fd8a7e7493724665d4098123404",
+    "url": "/Quiroga-sector1/static/js/2.760ae38c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c84cff7ce9a7105f9cfb",
+    "url": "/Quiroga-sector1/static/js/main.8df86664.chunk.js"
+  },
+  {
+    "revision": "c21fa9f3f835833a6a8e",
+    "url": "/Quiroga-sector1/static/js/runtime-main.c105f05c.js"
+  },
+  {
+    "revision": "1027ce84c7cdff14de9c51d12e659270",
+    "url": "/Quiroga-sector1/static/media/18010-Quiroga-I-sector (5).1027ce84.pdf"
+  },
+  {
+    "revision": "477d912876856794f3253caf8818cfb0",
+    "url": "/Quiroga-sector1/static/media/Instagram1.477d9128.png"
+  },
+  {
+    "revision": "80c67e6557dbc1599f8bbd0a4bdf73b6",
+    "url": "/Quiroga-sector1/static/media/LEY-743-DE-2002.80c67e65.pdf"
+  },
+  {
+    "revision": "91e96f67ec03b3945db0ae16f0aff950",
+    "url": "/Quiroga-sector1/static/media/LogoJunta.91e96f67.jpeg"
+  },
+  {
+    "revision": "ede75d1b75eb7a54765e92a677cfb883",
+    "url": "/Quiroga-sector1/static/media/RUribe.ede75d1b.jpg"
+  },
+  {
+    "revision": "2ef16b5b97e2f3532834737de015f0d3",
+    "url": "/Quiroga-sector1/static/media/WSP.2ef16b5b.png"
+  },
+  {
+    "revision": "1cc889aad4d2f900d2d2e112c654f231",
+    "url": "/Quiroga-sector1/static/media/arrowL.1cc889aa.jpg"
+  },
+  {
+    "revision": "3ea868fb5881aa95246c45ca187c6236",
+    "url": "/Quiroga-sector1/static/media/asado1.3ea868fb.jpeg"
+  },
+  {
+    "revision": "14da611ee420020ee6faadba06136e2f",
+    "url": "/Quiroga-sector1/static/media/asado2.14da611e.jpeg"
+  },
+  {
+    "revision": "2b056973f459b487356d477bcf876495",
+    "url": "/Quiroga-sector1/static/media/asado3.2b056973.jpeg"
+  },
+  {
+    "revision": "2b056973f459b487356d477bcf876495",
+    "url": "/Quiroga-sector1/static/media/asado4.2b056973.jpeg"
+  },
+  {
+    "revision": "18a5bd09c6710bd6c0db737cef16c4b1",
+    "url": "/Quiroga-sector1/static/media/asado5.18a5bd09.jpeg"
+  },
+  {
+    "revision": "1511eb0df577db7158a9c88f58665067",
+    "url": "/Quiroga-sector1/static/media/asojuntas.1511eb0d.jpg"
+  },
+  {
+    "revision": "fec0dc69b7aebc0531c3648359bab878",
+    "url": "/Quiroga-sector1/static/media/baile8.fec0dc69.jpeg"
+  },
+  {
+    "revision": "d1cf70f4944057b97d202a8ceba9fd0b",
+    "url": "/Quiroga-sector1/static/media/baile9.d1cf70f4.jpeg"
+  },
+  {
+    "revision": "b9938caa8bb0f32ff8629b015653c159",
+    "url": "/Quiroga-sector1/static/media/danza.b9938caa.jpeg"
+  },
+  {
+    "revision": "b9938caa8bb0f32ff8629b015653c159",
+    "url": "/Quiroga-sector1/static/media/danza2.b9938caa.jpeg"
+  },
+  {
+    "revision": "62c7da109cc7b628af2977002dbfe781",
+    "url": "/Quiroga-sector1/static/media/danza3.62c7da10.jpeg"
+  },
+  {
+    "revision": "fec0dc69b7aebc0531c3648359bab878",
+    "url": "/Quiroga-sector1/static/media/danza4.fec0dc69.jpeg"
+  },
+  {
+    "revision": "62c7da109cc7b628af2977002dbfe781",
+    "url": "/Quiroga-sector1/static/media/danza5.62c7da10.jpeg"
+  },
+  {
+    "revision": "4d8771063dbdf1d17be8968ebe07753f",
+    "url": "/Quiroga-sector1/static/media/fiesta.4d877106.jpeg"
+  },
+  {
+    "revision": "e875bfa41c0437f40c0089c656141310",
+    "url": "/Quiroga-sector1/static/media/fiesta1.e875bfa4.jpeg"
+  },
+  {
+    "revision": "62c7da109cc7b628af2977002dbfe781",
+    "url": "/Quiroga-sector1/static/media/fiesta3.62c7da10.jpeg"
+  },
+  {
+    "revision": "e875bfa41c0437f40c0089c656141310",
+    "url": "/Quiroga-sector1/static/media/fiesta4.e875bfa4.jpeg"
+  },
+  {
+    "revision": "e875bfa41c0437f40c0089c656141310",
+    "url": "/Quiroga-sector1/static/media/fiesta5.e875bfa4.jpeg"
+  },
+  {
+    "revision": "4d8771063dbdf1d17be8968ebe07753f",
+    "url": "/Quiroga-sector1/static/media/fiesta8.4d877106.jpeg"
+  },
+  {
+    "revision": "22256c03d237857f74c9669a45a32354",
+    "url": "/Quiroga-sector1/static/media/mirador.22256c03.jpg"
+  },
+  {
+    "revision": "ee0d7a5f821327ad567b8a246d594c02",
+    "url": "/Quiroga-sector1/static/media/principal.ee0d7a5f.jpeg"
+  },
+  {
+    "revision": "0290c48b6e6bc15114c86f84b6ea00cb",
+    "url": "/Quiroga-sector1/static/media/principal1.0290c48b.jpeg"
+  },
+  {
+    "revision": "d16cd29d6c2aade4b8d762df9eb89e91",
+    "url": "/Quiroga-sector1/static/media/r1.d16cd29d.jpeg"
+  },
+  {
+    "revision": "d16cd29d6c2aade4b8d762df9eb89e91",
+    "url": "/Quiroga-sector1/static/media/r2.d16cd29d.jpeg"
+  },
+  {
+    "revision": "d1cf70f4944057b97d202a8ceba9fd0b",
+    "url": "/Quiroga-sector1/static/media/r3.d1cf70f4.jpeg"
+  },
+  {
+    "revision": "9f25943d3e8d6a153fae4ff07865472e",
+    "url": "/Quiroga-sector1/static/media/twiter.9f25943d.png"
+  }
+]);
---
+++ precache-manifest.5d99f70a796a82293f6daf20c3067b43.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76179599980171c5d1f14ea9d458013b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aee6d6d3074078094f0",
+    "url": "/static/css/main.a8f7c1f1.chunk.css"
+  },
+  {
+    "revision": "330bcca011c18efaab13",
+    "url": "/static/js/2.c1370eb7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c1370eb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0aee6d6d3074078094f0",
+    "url": "/static/js/main.e987d876.chunk.js"
+  },
+  {
+    "revision": "a163fd723b86346545de",
+    "url": "/static/js/runtime-main.ecbdfc27.js"
+  },
+  {
+    "revision": "1027ce84c7cdff14de9c51d12e659270",
+    "url": "/static/media/18010-Quiroga-I-sector (5).1027ce84.pdf"
+  },
+  {
+    "revision": "477d912876856794f3253caf8818cfb0",
+    "url": "/static/media/Instagram1.477d9128.png"
+  },
+  {
+    "revision": "80c67e6557dbc1599f8bbd0a4bdf73b6",
+    "url": "/static/media/LEY-743-DE-2002.80c67e65.pdf"
+  },
+  {
+    "revision": "91e96f67ec03b3945db0ae16f0aff950",
+    "url": "/static/media/LogoJunta.91e96f67.jpeg"
+  },
+  {
+    "revision": "ede75d1b75eb7a54765e92a677cfb883",
+    "url": "/static/media/RUribe.ede75d1b.jpg"
+  },
+  {
+    "revision": "2ef16b5b97e2f3532834737de015f0d3",
+    "url": "/static/media/WSP.2ef16b5b.png"
+  },
+  {
+    "revision": "1cc889aad4d2f900d2d2e112c654f231",
+    "url": "/static/media/arrowL.1cc889aa.jpg"
+  },
+  {
+    "revision": "3ea868fb5881aa95246c45ca187c6236",
+    "url": "/static/media/asado1.3ea868fb.jpeg"
+  },
+  {
+    "revision": "14da611ee420020ee6faadba06136e2f",
+    "url": "/static/media/asado2.14da611e.jpeg"
+  },
+  {
+    "revision": "2b056973f459b487356d477bcf876495",
+    "url": "/static/media/asado3.2b056973.jpeg"
+  },
+  {
+    "revision": "2b056973f459b487356d477bcf876495",
+    "url": "/static/media/asado4.2b056973.jpeg"
+  },
+  {
+    "revision": "18a5bd09c6710bd6c0db737cef16c4b1",
+    "url": "/static/media/asado5.18a5bd09.jpeg"
+  },
+  {
+    "revision": "1511eb0df577db7158a9c88f58665067",
+    "url": "/static/media/asojuntas.1511eb0d.jpg"
+  },
+  {
+    "revision": "fec0dc69b7aebc0531c3648359bab878",
+    "url": "/static/media/baile8.fec0dc69.jpeg"
+  },
+  {
+    "revision": "d1cf70f4944057b97d202a8ceba9fd0b",
+    "url": "/static/media/baile9.d1cf70f4.jpeg"
+  },
+  {
+    "revision": "b9938caa8bb0f32ff8629b015653c159",
+    "url": "/static/media/danza.b9938caa.jpeg"
+  },
+  {
+    "revision": "b9938caa8bb0f32ff8629b015653c159",
+    "url": "/static/media/danza2.b9938caa.jpeg"
+  },
+  {
+    "revision": "62c7da109cc7b628af2977002dbfe781",
+    "url": "/static/media/danza3.62c7da10.jpeg"
+  },
+  {
+    "revision": "fec0dc69b7aebc0531c3648359bab878",
+    "url": "/static/media/danza4.fec0dc69.jpeg"
+  },
+  {
+    "revision": "62c7da109cc7b628af2977002dbfe781",
+    "url": "/static/media/danza5.62c7da10.jpeg"
+  },
+  {
+    "revision": "4d8771063dbdf1d17be8968ebe07753f",
+    "url": "/static/media/fiesta.4d877106.jpeg"
+  },
+  {
+    "revision": "e875bfa41c0437f40c0089c656141310",
+    "url": "/static/media/fiesta1.e875bfa4.jpeg"
+  },
+  {
+    "revision": "62c7da109cc7b628af2977002dbfe781",
+    "url": "/static/media/fiesta3.62c7da10.jpeg"
+  },
+  {
+    "revision": "e875bfa41c0437f40c0089c656141310",
+    "url": "/static/media/fiesta4.e875bfa4.jpeg"
+  },
+  {
+    "revision": "e875bfa41c0437f40c0089c656141310",
+    "url": "/static/media/fiesta5.e875bfa4.jpeg"
+  },
+  {
+    "revision": "4d8771063dbdf1d17be8968ebe07753f",
+    "url": "/static/media/fiesta8.4d877106.jpeg"
+  },
+  {
+    "revision": "22256c03d237857f74c9669a45a32354",
+    "url": "/static/media/mirador.22256c03.jpg"
+  },
+  {
+    "revision": "ee0d7a5f821327ad567b8a246d594c02",
+    "url": "/static/media/principal.ee0d7a5f.jpeg"
+  },
+  {
+    "revision": "0290c48b6e6bc15114c86f84b6ea00cb",
+    "url": "/static/media/principal1.0290c48b.jpeg"
+  },
+  {
+    "revision": "d16cd29d6c2aade4b8d762df9eb89e91",
+    "url": "/static/media/r1.d16cd29d.jpeg"
+  },
+  {
+    "revision": "d16cd29d6c2aade4b8d762df9eb89e91",
+    "url": "/static/media/r2.d16cd29d.jpeg"
+  },
+  {
+    "revision": "d1cf70f4944057b97d202a8ceba9fd0b",
+    "url": "/static/media/r3.d1cf70f4.jpeg"
+  },
+  {
+    "revision": "9f25943d3e8d6a153fae4ff07865472e",
+    "url": "/static/media/twiter.9f25943d.png"
+  }
+]);
